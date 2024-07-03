f452ad74c13d6c23bb29c79abd664ee1d3ab1373
daphne12345 <daphne.theo@yahoo.de>
2024-07-03 16:58:11 +0200

    commit

--- setup.py
+++ setup.py
@@ -6,9 +6,9 @@ setup(
     url='https://github.com/daphne12345/package_hpi_parego',
     author='Daphne',
     install_requires=['deepcave~=1.2.1', 'numpy==1.26.4', 'scipy~=1.14.0',
-        'smac~=2.1.0', 'pandas~=2.2.2', 'configspace~=0.7.1'],
+        'smac~=2.1.0', 'pandas~=2.2.2', 'configspace~=0.6.1'],
 
-    py_modules=['hpi_parego.my_config_selector', 'hpi_parego.my_ei_hpi',
-                'hpi_parego.my_local_and_random_search',
-                'hpi_parego.my_local_search', 'hpi_parego.fanova'],
+    py_modules=['my_config_selector', 'my_ei_hpi',
+                'my_local_and_random_search',
+                'my_local_search', 'fanova'],
 )
