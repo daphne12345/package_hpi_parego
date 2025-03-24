fix surrogate without variance

hpi_parego/fanova.py
@@ -19,14 +19,13 @@ class fANOVAWeighted(fANOVA):
 
     def train_model(
             self,
-            X, Y, weighting
+            X, Y
     ) -> None:
         """
         Train a FANOVA Forest model where the objectives are weighted by the input weighting.
         :param group: the runs as group
         :param df: dataframe containing the encoded data
         :param objectives_normed: the normalized objective names as a list of strings
-        :param weighting: the weighting as list
         """
         # X = df[group.configspace.get_hyperparameter_names()].to_numpy()
         if np.isnan(X).any():
@@ -36,8 +35,5 @@ class fANOVAWeighted(fANOVA):
 
             # Convert only integer-like columns back to int
             X[:, int_rows] = X[:, int_rows].astype(int)
-
-        Y = sum(obj * weighting for obj, weighting in zip(Y, weighting))
-
         self._model = FanovaForest(self.cs, n_trees=self.n_trees, seed=0)
         self._model.train(X, Y)

hypershap.py
@@ -14,7 +14,6 @@ class HPIGame(Game, ABC):
         cs,
         cfgs,
         model,
-        weighting,
         aggregator=lambda x: np.mean(np.array(x)),
         random_state=0,
         verbose = False,
@@ -25,7 +24,6 @@ class HPIGame(Game, ABC):
         self.cfgs = cfgs
         self.aggregator = aggregator
         self._model = model
-        self.weighting = weighting
         # determine empty coalition value for normalization
         super().__init__(
             n_players=len(cs.get_hyperparameters()),
@@ -37,8 +35,7 @@ class HPIGame(Game, ABC):
 
     def get_default_config_performance(self) -> float:
         X = convert_configurations_to_array([self.cs.get_default_configuration()])
-        Y = self._model.predict(X)
-        Y = sum(obj * weighting for obj, weighting in zip(Y, self.weighting))
+        Y, _ = self._model.predict(X)
         return self.aggregator(Y)
         
     def _before_first_value_function_hook(self):
@@ -63,8 +60,7 @@ class HPIGame(Game, ABC):
         cfgs = self.blind_parameters_according_to_coalition(self.cfgs, coalition)
         
         X = convert_configurations_to_array(cfgs)
-        Y = self._model.predict(X)
-        Y = sum(obj * weighting for obj, weighting in zip(Y, self.weighting))
+        Y,_ = self._model.predict(X)
         return self.aggregator(Y)
 
     def blind_parameters_according_to_coalition(self, cfgs, coalition):