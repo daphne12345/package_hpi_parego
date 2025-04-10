419c67297096481cdd6d892bb243e719f6c254ae
Daphne12345 <daphne.theo@yahoo.de>
2025-04-10 14:58:54 +0200

    job array?

--- job_array.sh
+++ job_array.sh
@@ -1,33 +1,72 @@
 #!/bin/bash
-#SBATCH --array 0%1
-#SBATCH --job-name hpi_parego
-#SBATCH --partition H100
-#SBATCH --gpus 1
-#BATCH ----cpus-per-gpu 8
-#SBATCH --time=1-00:00
-#SBATCH --mem=50G
+#SBATCH --array=0-99  # Adjust this range based on the number of different configurations (30 in this case)
+#SBATCH --job-name=multi_jobs
+#SBATCH --output=multi_jobs_%A_%a.out
+#SBATCH --error=multi_jobs_%A_%a.err
 
-seeds=("0" "0" "0" "0" "0" "1" "1" "1" "1" "1" "2" "2" "2" "2" "2" "3" "3" "3" "3" "3" "4" "4" "4" "4" "4")
+# Define all options for the different parameters
+hpi_method=("hypershap" "fanova" "random")
+adjust_cs=("true" "false")
+adjust_previous_cfgs=("true" "false")
+dynamic_decay=("linear" "none")
 
-problems=(
-  "subselection/multiobjective/dev=subset_hpobench_MO_tab_ml_rf_167119"
-)
+# Define conditional configurations
+adjust_cs_method=("default" "random")
+constant=("true" "false")
+set_to_default=("true" "false")
+thresh_linear=(0.9 0.8 0.7 0.6 0.5 0.4 0.3)
+thresh_no_decay=(0.5 0.75)
 
-#problems=('subselection/multiobjective/dev=subset_Pymoo_MO_omnitest' 'subselection/multiobjective/dev=subset_hpobench_MO_tab_ml_rf_167119' 'subselection/multiobjective/dev=subset_hpobench_MO_tab_ml_rf_31' 'subselection/multiobjective/dev=subset_hpobench_MO_tab_ml_svm_146212'
-# 'subselection/multiobjective/dev=subset_hpobench_MO_tab_ml_svm_146818' 'subselection/multiobjective/dev=subset_hpobench_MO_tab_ml_svm_31' 'subselection/multiobjective/dev=subset_hpobench_MO_tab_ml_xgboost_146822' 'subselection/multiobjective/dev=subset_hpobench_MO_tab_ml_xgboost_14965'
-#  'subselection/multiobjective/dev=subset_hpobench_MO_tab_ml_xgboost_167120' 'subselection/multiobjective/dev=subset_yahpo_mo_rbv2_rpart_40499_None')
+# Loop over all combinations of hpi_method, adjust_cs, adjust_previous_cfgs, and dynamic_decay
+index=0
+for hpi in "${hpi_method[@]}"; do
+    for acs in "${adjust_cs[@]}"; do
+        for adjust_prev in "${adjust_previous_cfgs[@]}"; do
+            for decay in "${dynamic_decay[@]}"; do
+                # Handle conditional configurations
+                if [ "$adjust_cs" == "true" ]; then
+                    for acs_method in "${adjust_cs_method[@]}"; do
+                        for cons in "${constant[@]}"; do
+                            baserundir="result_hpi_hypershap_adjust_cs_${acs}_method_${acs_method}_constant_${cons}"
+                            sbatch start_hydra.sh optimizer.smac_cfg.smac_kwargs.acquisition_maximizer.hpi_method=$hpi optimizer.smac_cfg.smac_kwargs.acquisition_maximizer.adjust_cs=$acs optimizer.smac_cfg.smac_kwargs.acquisition_maximizer.adjust_cs_method=$acs_method optimizer.smac_cfg.smac_kwargs.acquisition_maximizer.constant=$cons
+                        done
+                    done
+                fi
 
-# Get the command suffix for the current array index
-problem=${problems[$SLURM_ARRAY_TASK_ID - 1]}
-seed=${seeds[$SLURM_ARRAY_TASK_ID - 1]}
-echo ${problem}
-echo ${seed}
+                if [ "$acs" == "true" ] && [ "$adjust_prev" == "true" ]; then
+                    # Specific configuration for adjust_cs and adjust_previous_cfgs being true
+                    for set_default in "${set_to_default[@]}"; do
+                        baserundir="result_adjust_cs_${acs}_adjust_previous_${adjust_prev}_set_default_${set_default}"
+                        sbatch start_hydra.sh optimizer.smac_cfg.smac_kwargs.acquisition_maximizer.adjust_previous_cfgs=$adjust_prev optimizer.smac_cfg.smac_kwargs.acquisition_maximizer.set_to_default=$set_default
+                    done
+                fi
 
-srun \
-  --container-image=/netscratch/enroot/nvcr.io_nvidia_pytorch_23.12-py3.sqsh \
-  --container-workdir="`pwd`" \
-  --container-mounts=/netscratch/theodorakopoulos:/netscratch/theodorakopoulos,"`pwd`":"`pwd`" \
-  --task-prolog="`pwd`"/package_hpi_parego/install.sh \
-   python -m carps.run +optimizer/smac20=multiobjective +problem/${problem} seed=${seed}
+                if [ "$decay" == "linear" ]; then
+                    # Linear decay specific configurations
+                    for thresh in "${thresh_linear[@]}"; do
+                        baserundir="result_decay_linear_thresh_${thresh}"
+                        sbatch start_hydra.sh optimizer.smac_cfg.smac_kwargs.acquisition_maximizer.dynamic_decay=$decay optimizer.smac_cfg.smac_kwargs.acquisition_maximizer.thresh=$thresh
+                    done
+                elif [ "$decay" == "none" ]; then
+                    # No decay specific configurations
+                    for thresh in "${thresh_no_decay[@]}"; do
+                        baserundir="result_decay_none_thresh_${thresh}"
+                        sbatch start_hydra.sh optimizer.smac_cfg.smac_kwargs.acquisition_maximizer.dynamic_decay=$decay optimizer.smac_cfg.smac_kwargs.acquisition_maximizer.thresh=$thresh
+                    done
+                fi
 
-#   cmd="python -m carps.analysis.gather_data runs"
+                # Default configurations (for other combinations that don't match the conditionals)
+                if [ "$hpi" != "hypershap" ]; then
+                    baserundir="result_${hpi}_adjust_cs_${acs}_adjust_prev_${adjust_prev}_decay_${decay}"
+                    sbatch start_hydra.sh optimizer.smac_cfg.smac_kwargs.acquisition_maximizer.hpi_method=$hpi optimizer.smac_cfg.smac_kwargs.acquisition_maximizer.adjust_cs=$acs optimizer.smac_cfg.smac_kwargs.acquisition_maximizer.adjust_previous_cfgs=$adjust_prev optimizer.smac_cfg.smac_kwargs.acquisition_maximizer.dynamic_decay=$decay
+                fi
+
+                # Increment index
+                ((index++))
+                if [ $index -ge 30 ]; then
+                    exit 0  # Stop after 30 jobs to match the array size
+                fi
+            done
+        done
+    done
+done
